37e2fdb9c5defe2bc2f954bdb2126765c36c8785
Toram <shuniquely@gmail.com>
2022-08-30 02:40:51 +0900

    [Update] 포인터

---
+++ contents/김수한/수업정리/포인터/파일분할관리/Test1.h
@@ -0,0 +1,19 @@
+#pragma once // vs에서 사용 되는 문장
+
+#ifndef _TEST1_H__ // 옛날 방식
+#define _TEST1_H__
+
+struct StatInfo
+{
+	int hp;
+	int attack;
+	int defence;
+};
+
+void Test_1();
+
+void Test_2();
+
+void Test_3();
+
+#endif // _TEST1_H__
---
+++ contents/김수한/수업정리/포인터/파일분할관리/파일분할관리.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Test1.h"
+#include "Test1.h"
+#include "Test1.h"
+
+using namespace std;
+
+//  파일 분할 관리
+
+
+
+int main()
+{
+	Test_1();
+}
